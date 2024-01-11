b1e47e05889f4aaf87c218c8904bbf3f5b5502d2
ghyeet42 <carterfscarlett@gmail.com>
2024-01-11 10:53:12 -0500

    bruhhshhs

--- todo.py
+++ todo.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template, request, redirect, jsonify
 import pymysql
 import pymysql.cursors
 from pprint import pprint as print
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
+######
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 con = pymysql.connect (
     database = "cscarlett_todosah",
@@ -12,15 +18,25 @@ con = pymysql.connect (
     cursorclass = pymysql.cursors.DictCursor
 )
 
+######
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
 todolist = ["sleep", "scoot", "griddy"]
 
-######
-
-app = Flask(__name__)
-
-######
+########
 
 @app.route("/", methods = ["GET", "POST"])
+@auth.login_required
 def index():
 
     if request.method == "POST":
@@ -37,7 +53,6 @@ def index():
 
         con.commit()
 
-
     cursor = con.cursor()
 
     cursor.execute("SELECT * FROM `todosah` ORDER BY `complete`")
@@ -52,6 +67,8 @@ def index():
 
     # return ("<p style=\"color:red;\">Hello!</p>")
 
+#########
+
 @app.route("/completedTodo/<int:todoIndex>", methods = ["POST"])
 def todoCompleted(todoIndex):
      
@@ -63,9 +80,11 @@ def todoCompleted(todoIndex):
 
     con.commit()
 
+    #return "Hello, {}!".format(auth.current_user())
 
     return redirect("/")
 
+#########
 
 @app.route("/delete/<int:todoIndex>", methods = ["POST"])
 def todoDel(todoIndex):
